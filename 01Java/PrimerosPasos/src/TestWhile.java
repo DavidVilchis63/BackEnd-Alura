50ef40c348df62de2c64f955e33ec0c688ee22ae
David Vilchis2 <davidvilchis63@gmail.com>
2023-02-11 13:43:09 -0600

    Ciclos while y for

---
+++ 01Java/PrimerosPasos/src/TestWhile.java
@@ -0,0 +1,19 @@
+
+public class TestWhile {
+
+	public static void main(String[] args) {
+        int contador = 0;
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador ++;
+        }
+        
+        for(int contador1 = 0; contador1 <= 10; contador1++) {
+                    System.out.println(contador1);
+                
+        }
+
+        
+    }
+
+}
