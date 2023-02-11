b12c4f0a1dbb88f81b73b7c4f63e3e3255d46258
David Vilchis2 <davidvilchis63@gmail.com>
2023-02-11 13:57:19 -0600

    Ciclos break

---
+++ 01Java/PrimerosPasos/src/EjemploMatriz.java
@@ -0,0 +1,18 @@
+
+public class EjemploMatriz {
+	
+	public static void main(String[] args) {
+		
+		//Imprimir matriz cuadrada
+		
+		for (int fila = 0; fila <= 10; fila++) {
+			for (int columna = 0; columna < fila; columna++) {
+								
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
