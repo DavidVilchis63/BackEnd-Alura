a211c31233cbd7261448d6a34dd5c145a85b4839
David Vilchis2 <davidvilchis63@gmail.com>
2023-02-13 13:15:53 -0600

    Metodo retirar en la cuenta

--- 02Java POO/ProyectByteBank/src/Cuenta.java
+++ 02Java POO/ProyectByteBank/src/Cuenta.java
@@ -6,8 +6,21 @@ public class Cuenta {
 	int numero;
 	String titular;
 	
+	//No retorna valor
 	public void depositar (double valor) {
 		this.saldo = this.saldo + valor;
+	}
+	
+	//Retorna valor
+	public boolean retirar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
--- 02Java POO/ProyectByteBank/src/PruebaMetodos.java
+++ 02Java POO/ProyectByteBank/src/PruebaMetodos.java
@@ -3,10 +3,13 @@ public class PruebaMetodos {
 	
 	public static void main(String[] args) {
 		Cuenta cuentaDavid = new Cuenta();
-		cuentaDavid.saldo = 300;
 		
+		cuentaDavid.saldo = 300;		
 		cuentaDavid.depositar(500);
 		
+		System.out.println(cuentaDavid.saldo);
+		
+		cuentaDavid.retirar(100);
 		System.out.println(cuentaDavid.saldo);
 	}
 
