010d602fd639d3e6c3285e6e75bfb6ce03ec5c70
DavidVilchis <davidvilchis63@gmail.com>
2023-02-22 12:05:46 -0600

    Clase gerente y empezando con herencias

--- 02Java POO/ProyectByteBankHeredado/src/Funcionario.java
+++ 02Java POO/ProyectByteBankHeredado/src/Funcionario.java
@@ -4,6 +4,7 @@ public class Funcionario {
 	private String nombre;
 	private String documento;
 	private double salario;
+	private int tipo;
 	
 	public Funcionario() {
 		
@@ -29,8 +30,29 @@ public class Funcionario {
 	}
 	
 	public double getBonificacion() {
-		return this.salario * 0.1;
+		
+		if (this.tipo== 0) {
+			return this.salario * 0.1;
+		}
+		else if(this.tipo == 1) {
+			return this.salario;
+			
+		}
+		else {
+			return 0;
+		}
+		
 	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 	
 	
 }
--- 02Java POO/ProyectByteBankHeredado/src/TestFuncionario.java
+++ 02Java POO/ProyectByteBankHeredado/src/TestFuncionario.java
@@ -6,9 +6,17 @@ public class TestFuncionario {
 		diego.setDocumento("1123213");
 		diego.setNombre("Diego");
 		diego.setSalario(2000);
+		diego.setTipo(0);
+		
+		Funcionario gerente01 = new Funcionario();
+		gerente01.setSalario(5000);
+		gerente01.setTipo(1);
 		
 		System.out.println(diego.getSalario());
 		System.out.println(diego.getBonificacion());
+		
+		System.out.println(gerente01.getSalario());
+		System.out.println(gerente01.getBonificacion());
 		
 		
 	}
