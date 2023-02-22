977285dbffdd323df6a7a191bf80f45714868321
DavidVilchis <davidvilchis63@gmail.com>
2023-02-22 09:09:58 -0600

    Variables estaticas

--- 02Java POO/ProyectByteBank/src/Cuenta.java
+++ 02Java POO/ProyectByteBank/src/Cuenta.java
@@ -1,69 +1,71 @@
 
 public class Cuenta {
-	
+
 	private double saldo;
 	private int agencia;
 	int numero;
 	private Cliente titular = new Cliente();
 	
-	//public Cuenta() {
-	//	
-	//}
-	
+	private static int total = 0;
+
 	public Cuenta(int agencia) {
+
 		if (agencia <= 0) {
 			System.out.println("No se permite 0");
 			this.agencia = 1;
 		} else {
 			this.agencia = agencia;
 		}
-		System.out.println("Aqui se crea nueva cuenta");
+
+		total++;
+		System.out.println("Se van creando: " + total + " cuentas");
 	}
-	
-	//No retorna valor
-	public void depositar (double valor) {
+
+	// No retorna valor
+	public void depositar(double valor) {
 		this.saldo = this.saldo + valor;
 	}
-	
-	//Retorna valor
+
+	// Retorna valor
 	public boolean retirar(double valor) {
 		if (this.saldo >= valor) {
 			this.saldo = this.saldo - valor;
-			
+
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public boolean trasnferir(double valor, Cuenta cuenta) {
 		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			cuenta.depositar(valor);
-			
+
 			return true;
 		}
-		return false;		
+		return false;
 	}
-	
+
 	public double getSaldo() {
 		return this.saldo;
-		
+
 	}
-			
+
 	public int getAgencia() {
 		return agencia;
 	}
-	
+
 	public Cliente getTitular() {
 		return titular;
 	}
-	
 
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
 	
-	
+	public static int getTotal() {
+		return Cuenta.total;
+	}
+
 }
--- 02Java POO/ProyectByteBank/src/PruebaConstructor.java
+++ 02Java POO/ProyectByteBank/src/PruebaConstructor.java
@@ -2,7 +2,13 @@
 public class PruebaConstructor {
 
 	public static void main(String[] args) {
-		Cuenta cuentaUsuario5 = new Cuenta(8);		
+		Cuenta cuentaUsuario5 = new Cuenta(8);
+		Cuenta cuentaUsuario6 = new Cuenta(9);
+		Cuenta cuentaUsuario7 = new Cuenta(10);
 		System.out.println(cuentaUsuario5.getAgencia());
+		System.out.println(cuentaUsuario6.getAgencia());
+		System.out.println(cuentaUsuario7.getAgencia());
+		
+		System.out.println(Cuenta.getTotal()); 
 	}
 }
