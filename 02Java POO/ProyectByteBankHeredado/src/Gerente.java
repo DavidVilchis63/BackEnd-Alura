52a70776746cef8d223ea6db109827700dfc9a15
DavidVilchis <davidvilchis63@gmail.com>
2023-02-22 12:19:22 -0600

    Herencias con clases

--- 02Java POO/ProyectByteBankHeredado/src/Gerente.java
+++ 02Java POO/ProyectByteBankHeredado/src/Gerente.java
@@ -1,6 +1,19 @@
 
 public class Gerente extends Funcionario{
 
+	private String clave;
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	public boolean iniciarSesion(String clave) {
+		return clave == "Alura";
+	}
 	
 
 }
--- 02Java POO/ProyectByteBankHeredado/src/TestGerente.java
+++ 02Java POO/ProyectByteBankHeredado/src/TestGerente.java
@@ -1,4 +1,16 @@
 
 public class TestGerente {
+	
+	public static void main(String[] args) {
+
+		Gerente gerente02 = new Gerente();
+		gerente02.setSalario(5000);
+		gerente02.setTipo(1);
+		gerente02.setClave("Alura");
+		
+		System.out.println(gerente02.getSalario());
+		System.out.println(gerente02.getBonificacion());
+		System.out.println(gerente02.iniciarSesion("Alura"));
+	}
 
 }
